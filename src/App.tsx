001ffd145acccba603a952fcaf4ef863b6bf23fc
Eric Eichhorn <ee.eric.eichhorn@gmail.com>
2020-12-12 13:22:13 +0100

    created app

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const DecimalMilliseconds =
+  100 // hours
+  * 100 // minutes
+  * 100 // seconds
+  * 100 // milliseconds
+const NormalMilliseconds =
+  24 // hours
+  * 60 // minutes
+  * 60 // seconds
+  * 1000 // milliseconds
+
+export default class App extends React.Component {
+  private timeElement: HTMLParagraphElement | null | undefined
+
+  componentDidMount() {
+    this.animate()
+  }
+
+  private animate = () => {
+    if (this.timeElement) {
+      var date = new Date()
+      var milliseconds =
+        date.getMilliseconds()
+        + date.getSeconds() * 1000
+        + date.getMinutes() * 1000 * 60
+        + date.getHours() * 1000 * 60 * 60
+      var decimalDate = Math.round(milliseconds / NormalMilliseconds * DecimalMilliseconds)
+      var decimalDateString = decimalDate.toString()
+      var displayString =
+        decimalDateString.slice(0, 2)
+        + ":" + decimalDateString.slice(2, 4)
+        + ":" + decimalDateString.slice(4, 6)
+      this.timeElement.innerText = displayString
+    }
+    window.requestAnimationFrame(this.animate)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p ref={x => this.timeElement = x}>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    )
+  }
+}
